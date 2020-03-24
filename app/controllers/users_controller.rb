1cf85e8bc5d7c596643579ebd21a60b7ea048519
JeongWooYeong <wjd030811@gmail.com>
2020-03-24 17:54:45 +0900

    [ADD] 유저 회원가입 구현

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   before_action :oauth_required
 
   def create
+    return render status: 409 if User.find_by_email(@resp['email'])
 
+    user = User.create!(name: @resp['name'],
+                        email: @resp['email'])
+
+    render json: { user_id: user.id },
+           status: 201
   end
 end
