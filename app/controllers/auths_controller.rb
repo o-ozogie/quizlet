f9af6285fed1552af9417def9282b1dc73af2d7c
JeongWooYeong <wjd030811@gmail.com>
2020-03-24 17:46:17 +0900

    [ADD] 로그인, 리프레시 로직 작성

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,11 +1,18 @@
 class AuthsController < ApplicationController
-  before_action :oauth_required
+  before_action :oauth_required, only: :sign_in
+  before_action :refresh_token_required, only: :refresh
 
   def sign_in
+    user = User.find_by_email(@resp['email'])
+    return render status: 404 unless user
 
+    render json: { access_token: @@jwt_base.create_access_token(user_id: user.id),
+                   refresh_token: @@jwt_base.create_refresh_token(user_id: user.id)},
+           status: 200
   end
 
   def refresh
-
+    render json: { access_token: @@jwt_base.create_refresh_token(@payload) },
+           status: 200
   end
 end
