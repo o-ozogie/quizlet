7ec5947e492dbc10eba1a96de4f26816e17e5826
JeongWooYeong <wjd030811@gmail.com>
2020-03-24 13:52:17 +0900

    [ADD] 문제 컨트롤러 처리 로직 작성

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,18 +2,66 @@ class QuestionsController < ApplicationController
   before_action :jwt_required
 
   def create
+    requires(:score_second,
+             set_id: Integer,
+             description: String,
+             answer: String)
 
+    return render status: 404 unless QuestionSet.find_by_id(@params[:set_id])
+
+    Question.create!(question_set_id: @params[:set_id],
+                     description: @params[:description],
+                     answer: @params[:answer],
+                     score_second: @params[:score_second])
+
+    render status: 201
   end
 
   def show
+    requires(question_id: Integer)
 
+    question = Question.find_by_id(@params[:question_id])
+
+    render json: { question_id: question.id,
+                   question_set_id: question.question_set.id,
+                   description: question.description,
+                   created_at: question.created_at,
+                   updated_at: question.updated_at },
+           status: 200
   end
 
   def update
+    requires(:score_second,
+             question_id: Integer,
+             description: String,
+             answer: String)
 
+    question = Question.find_by_id(@params[:question_id])
+    return render status: 404 unless question
+    unless question.question_set.user.id == @payload['user_id']
+      return render status: 403
+    end
+
+    question.description = @params[:description] if @params[:description]
+    question.answer = @params[:answer] if @params[:answer]
+    question.score_second = @params unless @params[:score_second].nil?
+
+    question.save
+
+    render status: 200
   end
 
   def delete
+    requires(question_id: Integer)
 
+    question = Question.find_by_id(@params[:question_id])
+    return render status: 404 unless question
+    unless question.question_set.user.id == @payload['user_id']
+      return render status: 403
+    end
+
+    question.destroy!
+
+    render status: 200
   end
 end
